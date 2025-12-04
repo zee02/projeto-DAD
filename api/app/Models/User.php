8b09a3bc342401824571763fdcf78ffbf5193989
José Fernandes <zefrancisco2002@gmail.com>
2025-12-04 11:36:32 +0000

    Fix: Add photo_avatar_url accessor to User model for proper image URL serialization

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -15,6 +15,13 @@ class User extends Authenticatable
     use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
 
     /**
+     * The attributes that should be appended to the model's array form.
+     *
+     * @var list<string>
+     */
+    protected $appends = ['photo_avatar_url'];
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
@@ -54,6 +61,18 @@ class User extends Authenticatable
             'blocked' => 'boolean',
             'coins_balance' => 'integer',
         ];
+    }
+
+    /**
+     * Get the avatar URL from the filename
+     */
+    protected function photoAvatarUrl(): \Illuminate\Database\Eloquent\Casts\Attribute
+    {
+        return \Illuminate\Database\Eloquent\Casts\Attribute::make(
+            get: fn ($value) => $this->photo_avatar_filename 
+                ? url('storage/avatars/' . $this->photo_avatar_filename)
+                : null,
+        );
     }
 
     /**
